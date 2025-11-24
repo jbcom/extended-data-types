1696063a1f7360b74965dd650c1b75fb05115317
Jon Bogaty <jon@jonbogaty.com>
2025-11-24 04:05:51 -0600

    Implement transformation helpers and fix string utilities (#50)

---
+++ src/extended_data_types/transformations/numbers/__init__.py
@@ -0,0 +1,37 @@
+"""Transformations related to numeric formats and representations."""
+
+from .notation import (
+    from_fraction,
+    from_ordinal,
+    from_roman,
+    from_words,
+    to_fraction,
+    to_ordinal,
+    to_roman,
+    to_words,
+)
+from .words import (
+    fraction_to_words,
+    number_to_words,
+    ordinal_to_words,
+    words_to_fraction,
+    words_to_number,
+    words_to_ordinal,
+)
+
+__all__ = [
+    "from_fraction",
+    "from_ordinal",
+    "from_roman",
+    "from_words",
+    "to_fraction",
+    "to_ordinal",
+    "to_roman",
+    "to_words",
+    "fraction_to_words",
+    "number_to_words",
+    "ordinal_to_words",
+    "words_to_fraction",
+    "words_to_number",
+    "words_to_ordinal",
+]
